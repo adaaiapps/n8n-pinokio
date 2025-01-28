61facb07aab5d0e8cd5a30f2fcdcba8f7470f27b
angga riyansyah <angga.adaai@gmail.com>
2025-01-28 00:34:04 -0800

    Pesan commit untuk perubahan pinokio js

--- pinokio.js
+++ pinokio.js
@@ -1,105 +1,106 @@
 module.exports = {
-    title: "n8n",
-    description: "A powerful workflow automation tool.",
-    icon: "icon.png",
-    menu: async (kernel, info) => {
-      let installed = info.exists("app/node_modules");
-      let running = {
-        install: info.running("install.js"),
-        start: info.running("start.js"),
-        update: info.running("update.js"),
-        reset: info.running("reset.js"),
-      };
-  
-      if (running.install) {
-        return [
-          {
-            default: true,
-            icon: "fa-solid fa-plug",
-            text: "Installing",
-            href: "install.js",
-          },
-        ];
-      } else if (installed) {
-        if (running.start) {
-          let local = info.local("start.js");
-          if (local && local.url) {
-            return [
-              {
-                default: true,
-                icon: "fa-solid fa-rocket",
-                text: "Open Web UI",
-                href: local.url,
-              },
-              {
-                icon: "fa-solid fa-terminal",
-                text: "Terminal",
-                href: "start.js",
-              },
-            ];
-          } else {
-            return [
-              {
-                default: true,
-                icon: "fa-solid fa-terminal",
-                text: "Terminal",
-                href: "start.js",
-              },
-            ];
-          }
-        } else if (running.update) {
+  title: "n8n",
+  description: "A powerful workflow automation tool.",
+  icon: "icon.png",
+  menu: async (kernel, info) => {
+    let installed = false;
+    try {
+      const { stdout } = await kernel.shell.run({ message: "npm list -g n8n" });
+      installed = stdout.includes("n8n@");
+    } catch (e) {
+      // ignore
+    }
+
+    let running = {
+      install: info.running("install.js"),
+      start: info.running("start.js"),
+      update: info.running("update.js"),
+      reset: info.running("reset.js"),
+    };
+
+    if (running.install) {
+      return [
+        {
+          default: true,
+          icon: "fa-solid fa-plug",
+          text: "Installing",
+          href: "install.js",
+        },
+      ];
+    } else if (installed) {
+      if (running.start) {
+        let local = info.local("start.js");
+        if (local && local.url) {
           return [
             {
               default: true,
-              icon: "fa-solid fa-terminal",
-              text: "Updating",
-              href: "update.js",
+              icon: "fa-solid fa-rocket",
+              text: "Open Web UI",
+              href: local.url,
             },
-          ];
-        } else if (running.reset) {
-          return [
             {
-              default: true,
               icon: "fa-solid fa-terminal",
-              text: "Resetting",
-              href: "reset.js",
+              text: "Terminal",
+              href: "start.js",
             },
           ];
         } else {
           return [
             {
               default: true,
-              icon: "fa-solid fa-power-off",
-              text: "Start",
+              icon: "fa-solid fa-terminal",
+              text: "Terminal",
               href: "start.js",
-            },
-            {
-              icon: "fa-solid fa-plug",
-              text: "Update",
-              href: "update.js",
-            },
-            {
-              icon: "fa-solid fa-plug",
-              text: "Install",
-              href: "install.js",
-            },
-            {
-              icon: "fa-regular fa-circle-xmark",
-              text: "Reset",
-              href: "reset.js",
             },
           ];
         }
+      } else if (running.update) {
+        return [
+          {
+            default: true,
+            icon: "fa-solid fa-terminal",
+            text: "Updating",
+            href: "update.js",
+          },
+        ];
+      } else if (running.reset) {
+        return [
+          {
+            default: true,
+            icon: "fa-solid fa-terminal",
+            text: "Resetting",
+            href: "reset.js",
+          },
+        ];
       } else {
         return [
           {
             default: true,
+            icon: "fa-solid fa-power-off",
+            text: "Start",
+            href: "start.js",
+          },
+          {
             icon: "fa-solid fa-plug",
-            text: "Install",
-            href: "install.js",
+            text: "Update",
+            href: "update.js",
+          },
+          {
+            icon: "fa-regular fa-circle-xmark",
+            text: "Reset",
+            href: "reset.js",
           },
         ];
       }
-    },
-  };
-  
+    } else {
+      return [
+        {
+          default: true,
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        },
+      ];
+    }
+  },
+};

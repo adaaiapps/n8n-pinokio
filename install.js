ccdafc83bfad25c48aa8e2ef63bdf1637f135138
angga riyansyah <angga.adaai@gmail.com>
2025-01-27 22:30:32 -0800

    Fix install script to handle workspace dependencies

--- install.js
+++ install.js
@@ -1,33 +1,30 @@
 module.exports = {
-    run: [
-      {
-        method: "shell.run",
-        params: {
-          message: [
-            "git clone https://github.com/n8n-io/n8n app",
-          ],
-        },
+  run: [
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/n8n-io/n8n .",
+        ],
       },
-      {
-        method: "shell.run",
-        params: {
-          path: "app",
-          message: "npm install",
-        },
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: "npm install --legacy-peer-deps",
       },
-      {
-        method: "shell.run",
-        params: {
-          path: "app",
-          message: "npm run build",
-        },
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: "npm run build",
       },
-      {
-        method: "notify",
-        params: {
-          html: "Click the 'start' tab to get started!",
-        },
+    },
+    {
+      method: "notify",
+      params: {
+        html: "Click the 'start' tab to get started!",
       },
-    ],
-  };
-  
+    },
+  ],
+};
